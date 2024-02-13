db42728c01f87f906d9261d87c2efbb3eb5c76b4
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-13 10:45:19 +0500

    Add files via upload

---
+++ src/Layout/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+       <div className='bg-dark p-2' >
+        <h3 className='text-center p-3 text-white footerText'>Copyright &copy; by Liaqat Ali 2024. All Rights are Reserved</h3>
+      </div> 
+    </>
+  )
+}
+
+export default Footer
