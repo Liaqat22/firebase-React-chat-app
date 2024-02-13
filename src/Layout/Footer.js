6ce4a0ee2b0f974467c3837d86b7de80733d4b3c
Liaqat Ali <126480140+Liaqat22@users.noreply.github.com>
2024-02-13 12:03:44 +0500

    Update Footer.js

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <>
-       <div className='bg-dark p-2' >
+       <div className='bg-dark p-4' >
         <h3 className='text-center p-3 text-white footerText'>Copyright &copy; by Liaqat Ali 2024. All Rights are Reserved</h3>
       </div> 
     </>
